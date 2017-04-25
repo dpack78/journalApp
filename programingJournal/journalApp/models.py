3d7473e8c224d0e93ee75059a1654015520a091b
Dave Packer <dave78packer@gmail.com>
2017-04-24 23:31:05 -0600

    an admin added entry shows on the page

--- programingJournal/journalApp/models.py
+++ programingJournal/journalApp/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
+
+@python_2_unicode_compatible
+class Entry(models.Model):
+    creation_date = models.DateTimeField('date Created')
+    page_name = models.CharField(max_length=64)
+    line_number = models.CharField(max_length=5)
+    working_on = models.CharField(max_length=255)
+    goals = models.CharField(max_length=255)
+    def __str__(self):
+        return str(self.creation_date.strftime("%b %d %Y %H:%M"))
--- programingJournal/journalApp/views.py
+++ programingJournal/journalApp/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic import TemplateView
+from django.views.generic import DetailView
+from .models import Entry
+from django.utils import timezone
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = 'journalApp/index.html'
-    # context_object_name = 'lastEntry'
+# class IndexView():
+#     model = Entry
+#     slug_field = 'entry'
+#     template_name = 'journalApp/index.html'
+#     # context_object_name = 'lastEntry'
+#     # newest_entry = Entry.objects.latest('creation_date')
+#
+#     # def getMostRecentEntry():
+#         # return Entry.objects.filter('field')
+#     def get_queryset(self):
+#         return Entry.objects.latest('creation_date')
 
-    # def get_queryset(self):
-    #     return "TEST";
+# def saveCodeSession(request):
+
+def index(request):
+    newest_entry = Entry.objects.latest('creation_date');
+    context = { 'newest_entry': newest_entry}
+    return render(request, 'journalApp/index.html',context)
