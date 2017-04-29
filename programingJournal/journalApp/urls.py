712028922f60950250ed82e2d4451aca6cba16d5
Dave Packer <dave78packer@gmail.com>
2017-04-29 12:39:06 -0600

    final local commit before adding to cloud9

--- programingJournal/journalApp/urls.py
+++ programingJournal/journalApp/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'journalApp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.index, name='index'),
     url(r'^/saveNewDay',views.saveNewDay, name='saveNewDay')
 ]
--- programingJournal/journalApp/views.py
+++ programingJournal/journalApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.views.generic import DetailView
 from .models import Entry
@@ -9,39 +9,25 @@ from itertools import chain
 import pdb
 # Create your views here.
 
-# class IndexView():
-#     model = Entry
-#     slug_field = 'entry'
-#     template_name = 'journalApp/index.html'
-#     # context_object_name = 'lastEntry'
-#     # newest_entry = Entry.objects.latest('creation_date')
-#
-#     # def getMostRecentEntry():
-#         # return Entry.objects.filter('field')
-#     def get_queryset(self):
-#         return Entry.objects.latest('creation_date')
+def index(request, pk = -1):
+    entry_id = pk
+    if entry_id != -1:
+        newest_entry = get_object_or_404(Entry, pk=entry_id)
+    else:
+        newest_entry = Entry.objects.latest('creation_date')
 
-# def saveCodeSession(request):
-
-def index(request):
-    newest_entry = Entry.objects.latest('creation_date')
-	all_entries = Entry.objects.all()
-    context = { 'newest_entry': newest_entry,
-				'all_entries' : all_entries
-	}
+    all_entries = Entry.objects.all()
+    context = { 'newest_entry' : newest_entry,
+                'all_entries' : all_entries
+    }
     return render(request, 'journalApp/index.html',context)
 
 def saveNewDay(request):
     entry = Entry()
-    # pdb.set_trace()
     entry.creation_date = timezone.now()
     entry.page_name = request.GET['curPage']
-    # ('curPage',"None")
     entry.line_number = request.GET['lineNumber']
-    # ('lineNumber',-1)
     entry.working_on = request.GET['currentThoughts']
-    # ('currentThoughts',"None")
     entry.goals = request.GET['goals']
-    # ('goals',"No Goals")
     entry.save()
     return HttpResponseRedirect('/')
